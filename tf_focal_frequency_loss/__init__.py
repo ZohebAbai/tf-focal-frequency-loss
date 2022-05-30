cf5d4c91bc705bdc57e87f3b94ad1ead19a9ad0a
Zoheb Abai <zohebabai@gmail.com>
2022-05-30 14:58:55 +0530

    update 0.1.2

--- tf_focal_frequency_loss/__init__.py
+++ tf_focal_frequency_loss/__init__.py
@@ -1,5 +1,5 @@
 """Focal Frequency Loss for Image Reconstruction and Synthesis - Tensorflow Implementation"""
 
-__version__ = '0.1.0'
+__version__ = '0.1.2'
 
 from .tf_focal_frequency_loss import FocalFrequencyLoss
